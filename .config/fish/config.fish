8c0d50760b53d673832f02a87681d066c90bb9e8
soblin <hilo.soblin@gmail.com>
2022-05-31 21:59:28 +0900

    removed colcon_all and colcon_select from custom/bin
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,11 @@ set -g -x PATH ~/.local/bin/custom $PATH
 alias colcon='__colcon_find_workspace_dir > /dev/null && cd (__colcon_find_workspace_dir); command colcon'
 alias roscd="ccd -o"
 register-python-argcomplete --shell fish ros2 | source
+
+function colcon_all
+   colcon build --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+end
+
+function colcon_select
+   colcon build --packages-up-to $argv
+end
