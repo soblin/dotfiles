79022e166d7af5f9e1f2e304d19adbfac6a49794
soblin <hilo.soblin@gmail.com>
2022-06-24 18:46:45 +0900

    modified colcon to use ccache
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,11 +18,11 @@ alias roscd="ccd -o"
 register-python-argcomplete --shell fish ros2 | source
 
 function colcon_all
-   colcon build --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+   colcon build --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc
 end
 
 function colcon_select
-   colcon build --packages-up-to $argv --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+   colcon build --packages-up-to $argv --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc
 end
 
 function psgrep
