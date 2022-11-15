87570ca5ba9dc765b3b0956b85170b993cde9f50
soblin <hilo.soblin@gmail.com>
2022-11-15 15:15:47 +0900

    cmake-clean-cache slows down compilation
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/configs/ros2.fish
+++ .config/fish/configs/ros2.fish
@@ -3,10 +3,11 @@ alias colcon='__colcon_find_workspace_dir > /dev/null && cd (__colcon_find_works
 alias roscd="ccd -o"
 register-python-argcomplete --shell fish ros2 | source
 
+# --cmake-clean-cache slows down compilation
 function colcon_all
-    colcon build --cmake-clean-cache --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations"
+    colcon build --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations"
 end
 
 function colcon_select
-    colcon build --packages-up-to $argv --cmake-clean-cache --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations"
+    colcon build --packages-up-to $argv --symlink-install --continue-on-error --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations"
 end
