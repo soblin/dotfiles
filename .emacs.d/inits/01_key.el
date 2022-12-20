634780cbdbf2e334a7cad670e28ddc99799c42e9
soblin <hilo.soblin@gmail.com>
2022-12-20 16:26:37 +0900

    C-y for helm-show-kill-ring
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .emacs.d/inits/01_key.el
+++ .emacs.d/inits/01_key.el
@@ -50,7 +50,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
