0818c119255c8a7deae3ce69f4e4ee9f387b5b3b
soblin <hilo.soblin@gmail.com>
2023-04-16 06:35:57 +0900

    use rust-analyzer instead of RLS because eglot does not support it
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .emacs.d/inits/31_rust.el
+++ .emacs.d/inits/31_rust.el
@@ -10,7 +10,11 @@
 
 (setq rust-format-on-save t)
 
-(add-hook 'rust-mode-hook 'eglot-ensure)
+;; (add-hook 'rust-mode-hook 'eglot-ensure)
+;; eglot does not support rust-analyzer, so use lsp-mode
+;; https://rust-analyzer.github.io/manual.html#lsp-mode
+;; install rust-analyzer: https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
+(add-hook 'rust-mode-hook 'lsp-deferred)
 
 ;;; End:
 
