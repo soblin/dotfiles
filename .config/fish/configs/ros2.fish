60a29a51b96554cbca4c76295ee8f27cfea29e69
soblin <hilo.soblin@gmail.com>
2024-06-18 18:41:18 +0900

    update colcon (2)
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/configs/ros2.fish
+++ .config/fish/configs/ros2.fish
@@ -6,7 +6,7 @@ if command -v ros2 &> /dev/null
 
     # --cmake-clean-cache slows down compilation
     function colcon_all
-        colcon build --symlink-install --continue-on-error --event-handlers console_cohesion+ --cmake-args " -GNinja" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations"
+        colcon build --symlink-install --continue-on-error --event-handlers console_cohesion+ --cmake-args " -GNinja" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -Wno-unknown-pragmas"
     end
 
     function colcon_select_upto
