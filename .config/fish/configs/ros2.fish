c2bd50f82dfc8adff9999715a242d5f958f5da86
soblin <hilo.soblin@gmail.com>
2025-03-03 03:02:33 +0900

    update colcon_test_coverage
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/configs/ros2.fish
+++ .config/fish/configs/ros2.fish
@@ -23,7 +23,8 @@ if command -v ros2 &> /dev/null
     end
 
     function colcon_test_coverage
-        colcon build --packages-up-to $argv --symlink-install --continue-on-error --event-handlers console_cohesion+ --cmake-args " -GNinja" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -Wno-unknown-pragmas -fprofile-arcs -ftest-coverage"
+        colcon build --packages-select $argv --symlink-install --continue-on-error --event-handlers console_cohesion+ --cmake-args " -GNinja" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -Wno-unknown-pragmas -fprofile-arcs -ftest-coverage"
         colcon test --event-handlers console_cohesion+ --packages-select $argv
+        colcon lcov-result --packages-select $argv --filter "*/test/*"
     end
 end
