d847b634c676932894ffdc1b34b2717d86a74b5e
soblin <hilo.soblin@gmail.com>
2025-09-16 10:18:00 +0900

    fix colcon commands to terminate on fail
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/configs/ros2.fish
+++ .config/fish/configs/ros2.fish
@@ -18,13 +18,13 @@ if command -v ros2 &> /dev/null
     end
 
     function colcon_test_simple
-        colcon build --packages-select $argv --symlink-install --continue-on-error --event-handlers console_cohesion+ --cmake-args " -GNinja" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -Wno-unknown-pragmas"
+        colcon build --packages-select $argv --symlink-install --continue-on-error --event-handlers console_cohesion+ --cmake-args " -GNinja" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -Wno-unknown-pragmas"; and \
         colcon test --event-handlers console_cohesion+ --packages-select $argv
     end
 
     function colcon_test_coverage
-        colcon build --packages-select $argv --symlink-install --continue-on-error --event-handlers console_cohesion+ --cmake-args " -GNinja" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -Wno-unknown-pragmas -fprofile-arcs -ftest-coverage"
-        colcon test --event-handlers console_cohesion+ --packages-select $argv
+        colcon build --packages-select $argv --symlink-install --continue-on-error --event-handlers console_cohesion+ --cmake-args " -GNinja" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -Wno-unknown-pragmas -fprofile-arcs -ftest-coverage"; and \
+        colcon test --event-handlers console_cohesion+ --packages-select $argv; and \
         colcon lcov-result --packages-select $argv --filter "*/test/*"
     end
 end
