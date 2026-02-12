9a086dd6b6e491c4ec6e072cb76c6fa2130b3764
soblin <hilo.soblin@gmail.com>
2026-02-13 07:24:58 +0900

    refactor(peco): refactor for private function
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/configs/peco_anything.fish
+++ .config/fish/configs/peco_anything.fish
@@ -6,7 +6,7 @@ set -l pink_violet 141
 set -l pink 212
 set -l black 237
 
-function peco_complete_cmd_anything
+function __peco_complete_cmd_anything
     set -l tokens (commandline --tokenize)
 
     set -l len (count $tokens)
@@ -17,18 +17,18 @@ function peco_complete_cmd_anything
         set -l token $tokens[$i]
         switch "$token"
             case git
-                peco_git_dispatch $tokens[$i..$len]
+                __peco_git_dispatch $tokens[$i..$len]
             case cd
-                peco_cd_dispatch
+                __peco_cd_dispatch
             case '*'
                 return
         end
     end
 end
 
-bind \cg peco_complete_cmd_anything
+bind \cg __peco_complete_cmd_anything
 
-function create_dracula_theme_prompt
+function __create_dracula_theme_prompt
     set -l sub_prompt $argv
     set -l gray_blue color60
     set -l dark_gray_blue color238
@@ -39,7 +39,7 @@ function create_dracula_theme_prompt
     printf " #[fg=%s, bg=%s, bold] %s #[fg=%s, bg=%s]%s#[fg=%s, bg=%s, bold] %s #[fg=%s, bg=default]%s " $black $pink_violet $peco_anything_prompt $pink_violet $pink $separator $gray_blue $pink $sub_prompt $pink $separator
 end
 
-function call_cmd_peco_tmux_popup
+function __call_cmd_peco_tmux_popup
     set -l list_cmds $argv[1]
     set -l cmd_len (math (count $argv) - 2)
     if test $cmd_len -gt 1
@@ -93,62 +93,62 @@ function call_cmd_peco_tmux_popup
     end
 end
 
-function peco_git_unstaged_file_dir
+function __peco_git_unstaged_file_dir
     set -l sub_prompt " git unstaged files "
-    set -l popup_status (create_dracula_theme_prompt $sub_prompt)
+    set -l popup_status (__create_dracula_theme_prompt $sub_prompt)
     set -l list_cmds "git status -uall --porcelain"
     set -l select_cmd "awk '{print \$NF}'"
 
-    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    set -l target (__call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
     test -z "$target"; and return
     commandline --insert -- "$target"
 end
 
-function peco_git_branch
+function __peco_git_branch
     set -l sub_prompt " git branches "
-    set -l popup_status (create_dracula_theme_prompt $sub_prompt)
+    set -l popup_status (__create_dracula_theme_prompt $sub_prompt)
     set -l list_cmds "git branch --format='%(refname:short)'"
     set -l select_cmd ""
 
-    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    set -l target (__call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
     test -z "$target"; and return
     commandline --insert -- "$target"
 end
 
-function peco_git_branch
+function __peco_git_tag
     set -l sub_prompt " git tag "
-    set -l popup_status (create_dracula_theme_prompt $sub_prompt)
+    set -l popup_status (__create_dracula_theme_prompt $sub_prompt)
     set -l list_cmds "git tag"
     set -l select_cmd ""
 
-    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    set -l target (__call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
     test -z "$target"; and return
     commandline --insert -- "$target"
 end
 
-function peco_git_branch_or_tag
+function __peco_git_branch_or_tag
     set -l sub_prompt " git branches  / tags "
-    set -l popup_status (create_dracula_theme_prompt $sub_prompt)
+    set -l popup_status (__create_dracula_theme_prompt $sub_prompt)
     set -l list_cmds "git branch --format='%(refname:short)'" "git tag"
     set -l select_cmd "sort -u"
 
-    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    set -l target (__call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
     test -z "$target"; and return
     commandline --insert -- "$target"
 end
 
-function peco_git_remote
+function __peco_git_remote
     set -l sub_prompt " git remotes 󰖟"
-    set -l popup_status (create_dracula_theme_prompt $sub_prompt)
+    set -l popup_status (__create_dracula_theme_prompt $sub_prompt)
     set -l list_cmds "git remote show"
     set -l select_cmd ""
 
-    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    set -l target (__call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
     test -z "$target"; and return
     commandline --insert -- "$target"
 end
 
-function peco_git_dispatch
+function __peco_git_dispatch
     set -l len (count $argv)
 
     test $len -lt 2; and return
@@ -157,30 +157,30 @@ function peco_git_dispatch
 
     switch "$subcmd"
         case add
-            peco_git_unstaged_file_dir
+            __peco_git_unstaged_file_dir
         case branch merge rebase
-            peco_git_branch
+            __peco_git_branch
         case tag
-            peco_git_tag
+            __peco_git_tag
         case push
             # TODO: if argc == 2, complete remote, and if 3, complete branch/tag
-            peco_git_branch_or_tag
+            __peco_git_branch_or_tag
         case checkout switch diff log
-            peco_git_branch_or_tag
+            __peco_git_branch_or_tag
         case remote
-            peco_git_remote
+            __peco_git_remote
         case '*'
             return
     end
 end
 
-function peco_cd_dispatch
+function __peco_cd_dispatch
     set -l sub_prompt " ls/bd/z candidates 󰔰"
-    set -l popup_status (create_dracula_theme_prompt $sub_prompt)
+    set -l popup_status (__create_dracula_theme_prompt $sub_prompt)
     set -l list_cmds "complete -C 'cd '" "complete -C 'bd '" "complete -C 'z '"
     set -l select_cmd "xargs realpath"
 
-    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    set -l target (__call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
     test -z "$target"; and return
     commandline --insert -- "$target"
 end
