a05d5fa870e138d19255685038b11fce493cac23
Mamoru Sobue <hilo.soblin@gmail.com>
2026-02-13 06:59:12 +0900

    feat(peco): make multiple list command work, add `cd` completion (#65)
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/configs/peco_anything.fish
+++ .config/fish/configs/peco_anything.fish
@@ -18,6 +18,8 @@ function peco_complete_cmd_anything
         switch "$token"
             case git
                 peco_git_dispatch $tokens[$i..$len]
+            case cd
+                peco_cd_dispatch
             case '*'
                 return
         end
@@ -39,22 +41,52 @@ end
 
 function call_cmd_peco_tmux_popup
     set -l list_cmds $argv[1]
-    set -l select_cmd $argv[2]
-    set -l popup_status $argv[3]
+    set -l cmd_len (math (count $argv) - 2)
+    if test $cmd_len -gt 1
+        set list_cmds $argv[1..$cmd_len]
+    end
+    set -l select_cmd $argv[-2]
+    set -l popup_status $argv[-1]
 
     set -l tmp (mktemp)
-
-    set -l cmd (string join "\n" $list_cmds)
     # if $select_cmd == "", | | is invalid
     set -l filter_pipe ""
     test -n "$select_cmd"; and set filter_pipe " | $select_cmd"
 
+    # argc == 1
     set -l popup_cmd "
     begin
-        $cmd
+        $argv[1]
     end $filter_pipe |
     peco --prompt '(i-search)`\'' > $tmp
     "
+
+    # argc == 2
+    if test $cmd_len -eq 2
+        set -l cmds (string join "\n    " $list_cmds)
+        set popup_cmd "
+        begin
+            # NOTE: $cmds does not work. Why?
+            $list_cmds[1]
+            $list_cmds[2]
+        end $filter_pipe |
+        peco --prompt '(i-search)`\'' > $tmp
+        "
+    end
+
+    # argc == 3
+    if test $cmd_len -eq 3
+        set popup_cmd "
+        begin
+            # NOTE: $cmds does not work
+            $list_cmds[1]
+            $list_cmds[2]
+            $list_cmds[3]
+        end $filter_pipe |
+        peco --prompt '(i-search)`\'' > $tmp
+        "
+    end
+
     tmux display-popup -E -T "$popup_status" -d (pwd) "$popup_cmd"
     if test -s $tmp
         printf (cat $tmp)
@@ -89,9 +121,9 @@ function peco_git_branch_or_tag
     set -l list_cmds "git branch --format='%(refname:short)'" "git tag"
     set -l select_cmd "sort -u"
 
-    set -l remote (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
-    test -z "$remote"; and return
-    commandline --insert -- "$remote"
+    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    test -z "$target"; and return
+    commandline --insert -- "$target"
 end
 
 function peco_git_remote
@@ -100,9 +132,9 @@ function peco_git_remote
     set -l list_cmds "git remote show"
     set -l select_cmd ""
 
-    set -l remote (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
-    test -z "$remote"; and return
-    commandline --insert -- "$remote"
+    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    test -z "$target"; and return
+    commandline --insert -- "$target"
 end
 
 function peco_git_dispatch
@@ -127,4 +159,15 @@ function peco_git_dispatch
         case '*'
             return
     end
+end
+
+function peco_cd_dispatch
+    set -l sub_prompt " ls/bd/z candidates 󰔰"
+    set -l popup_status (create_dracula_theme_prompt $sub_prompt)
+    set -l list_cmds "complete -C 'cd '" "complete -C 'bd '" "complete -C 'z '"
+    set -l select_cmd "xargs realpath"
+
+    set -l target (call_cmd_peco_tmux_popup $list_cmds $select_cmd $popup_status)
+    test -z "$target"; and return
+    commandline --insert -- "$target"
 end
