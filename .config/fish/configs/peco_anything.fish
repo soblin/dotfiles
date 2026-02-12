1ae73463e4de594b7b4acf745f3a0a51080858da
soblin <hilo.soblin@gmail.com>
2026-02-13 06:30:11 +0900

    argv = 2 works
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- .config/fish/configs/peco_anything.fish
+++ .config/fish/configs/peco_anything.fish
@@ -41,27 +41,34 @@ end
 
 function call_cmd_peco_tmux_popup
     set -l list_cmds $argv[1]
-    set -l len (count $argv)
-    if test $len -gt 3
-        set list_cmds $argv[1..(math $len - 2)]
+    set -l cmd_len (math (count $argv) - 2)
+    if test $cmd_len -gt 1
+        set list_cmds $argv[1..$cmd_len]
     end
     set -l select_cmd $argv[-2]
     set -l popup_status $argv[-1]
 
     set -l tmp (mktemp)
-
-    set -l cmd (string join "\n" $list_cmds)
     # if $select_cmd == "", | | is invalid
     set -l filter_pipe ""
     test -n "$select_cmd"; and set filter_pipe " | $select_cmd"
 
     set -l popup_cmd "
     begin
-        git branch --format='%(refname:short)'
-        git tag
+        $argv[1]
     end $filter_pipe |
     peco --prompt '(i-search)`\'' > $tmp
     "
+    if test $cmd_len -gt 1
+        set -l cmds (string join "\n    " $list_cmds)
+        set popup_cmd "
+        begin
+            $list_cmds[1]
+            $list_cmds[2]
+        end $filter_pipe |
+        peco --prompt '(i-search)`\'' > $tmp
+        "
+    end
     tmux display-popup -E -T "$popup_status" -d (pwd) "$popup_cmd"
     if test -s $tmp
         printf (cat $tmp)
