b2a7ad098de4244b5407dd787c57e1b0b52dd73a
soblin <hilo.soblin@gmail.com>
2026-02-16 01:50:22 +0900

    feat(tmux): add gpu monitor
    Signed-off-by: soblin <hilo.soblin@gmail.com>

---
+++ .config/tmux/scripts/gpu_monitor.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+gpu=$(lspci -v | grep VGA | head -n 1 | awk '{print $5}')
+
+monitor_nvidia() {
+	power=$(nvidia-smi --query-gpu=power.draw,power.limit --format=csv,noheader,nounits | awk -F ', *' '{ printf("%.2f%%", $0 / $2 * 100) }')
+	vram=$(nvidia-smi --query-gpu=memory.used,memory.total --format=csv,noheader,nounits | awk -F ', *' '{ printf("%.2f%%", $0 / $2 * 100) }')
+	usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk -F ', *' '{ printf("%.2f%%", $1) }')
+	echo "󰢮 $1 | ⏻  ${power} |   ${vram} | 󰊚  ${usage}"
+}
+
+monitor() {
+	if [[ "$gpu" == "NVIDIA" ]]; then
+		monitor_nvidia "NVIDIA"
+	else
+		echo "No GPU"
+	fi
+	sleep $1
+}
+
+main() {
+	rate=0.1
+	while [[ $# -gt 0 ]]; do
+		case $1 in
+		-r | --rate)
+			rate=$2
+			shift
+			shift
+			;;
+		*)
+			break
+			;;
+		esac
+	done
+
+	monitor $rate
+}
+
+main $@
